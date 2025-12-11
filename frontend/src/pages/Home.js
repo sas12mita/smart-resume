ee55fd7af9b9cdc839272720d7db38da3f9a699b
sas12mita <sumitapoudel12@gmail.com>
2025-12-11 16:59:47 +0545

    frontend debugging

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,11 @@
-import Navbar from "../components/navbar";
+import React from "react";
 import HeroSection from "../components/HeroSection";
-
+import Navbar from "../components/Navbar";
 function Home() {
-  return (
-    <>
-      <Navbar />
-      <HeroSection />
-    </>
-  );
-}
+  return <>gi
+    <Navbar />
+    <HeroSection />
 
+  </>;
+}
 export default Home;
